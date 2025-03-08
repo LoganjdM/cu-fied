713dd447bb982a125cfce9f54291fd5bed53e658
Logan M- <logan@mccuistion.my>
2025-03-07 22:09:53 -0600

    make the "polyfill"(ive genuinely never heard of this term in my life until Eli started talkin about it) look fancier

--- src/polyfill/mempcpy.h
+++ src/polyfill/mempcpy.h
@@ -5,8 +5,7 @@
 
 // Only define mempcpy if it's not already available
 #if defined(__APPLE__) && TARGET_OS_MAC==1 || !defined(__GLIBC__)
-static inline void *
-mempcpy(void *dest, const void *src, size_t n) {
+static inline void* mempcpy(void *dest, const void *src, size_t n) {
     memcpy(dest, src, n);
     return (char*)dest + n;
 }
--- src/polyfill/reallocarray.h
+++ src/polyfill/reallocarray.h
@@ -8,8 +8,7 @@
 #if defined(__APPLE__) && TARGET_OS_MAC==1 || !defined(__GLIBC__)
 #define MUL_NO_OVERFLOW ((size_t)1 << (sizeof(size_t) * 4))
 
-static inline void *
-reallocarray(void *optr, size_t nmemb, size_t size)
+static inline void* reallocarray(void *optr, size_t nmemb, size_t size)
 {
     if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) &&
             nmemb > 0 && SIZE_MAX / nmemb < size) {
