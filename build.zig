b81ffb3fca28d54401a50722c117d9ca4cd24425
Logan M- <logan@mccuistion.my>
2025-03-08 11:36:51 -0600

    zig fmt

--- build.zig
+++ build.zig
@@ -6,64 +6,46 @@ const proc = std.process;
 
 var global_check: *Build.Step = undefined;
 fn add_build_steps(b: *Build, name: []const u8, check_exe: *Build.Step, run_exe: *Build.Step) !void {
-	var gpa: std.heap.DebugAllocator(.{}) = .init;
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer std.debug.assert(gpa.deinit() == .ok);
     const alloc = gpa.allocator();
 
-	const check_str = try std.fmt.allocPrint(alloc, "check-{s}", .{name});
-	const check_desc_str = try std.fmt.allocPrint(alloc, "See if {s} compiles", .{name});
-	defer alloc.free(check_str); defer alloc.free(check_desc_str);
-	
-	const check = b.step(check_str, check_desc_str);
-	check.dependOn(check_exe);
-	global_check.dependOn(check_exe);
-	
-	const run_str = try std.fmt.allocPrint(alloc, "run-{s}", .{name});
-	const run_desc_str = try std.fmt.allocPrint(alloc, "Run {s}", .{name});
-	defer alloc.free(run_str); defer alloc.free(run_desc_str);	
+    const check_str = try std.fmt.allocPrint(alloc, "check-{s}", .{name});
+    const check_desc_str = try std.fmt.allocPrint(alloc, "See if {s} compiles", .{name});
+    defer alloc.free(check_str);
+    defer alloc.free(check_desc_str);
 
-	const run = b.step(run_str, run_desc_str);
-	run.dependOn(run_exe);	
+    const check = b.step(check_str, check_desc_str);
+    check.dependOn(check_exe);
+    global_check.dependOn(check_exe);
+
+    const run_str = try std.fmt.allocPrint(alloc, "run-{s}", .{name});
+    const run_desc_str = try std.fmt.allocPrint(alloc, "Run {s}", .{name});
+    defer alloc.free(run_str);
+    defer alloc.free(run_desc_str);
+
+    const run = b.step(run_str, run_desc_str);
+    run.dependOn(run_exe);
 }
 
 var cflags: [3][]const u8 = undefined;
 fn build_c(b: *Build, srcs: anytype, target: Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, name: []const u8) !void {
-	const exe = b.addExecutable(.{
-		.name = name,
-		.root_module = b.createModule(.{
-			.target = target,
-			.optimize = optimize,
-			.link_libc = true
-		})
-	});
+    const exe = b.addExecutable(.{ .name = name, .root_module = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true }) });
 
-	exe.root_module.addCSourceFiles(.{
-		.files = srcs,
-		.flags = &cflags
-	});
-	b.installArtifact(exe);
+    exe.root_module.addCSourceFiles(.{ .files = srcs, .flags = &cflags });
+    b.installArtifact(exe);
 
-	const exe_check = b.addExecutable(.{
-		.name = name,
-		.root_module = b.createModule(.{
-			.target = target,
-			.optimize = optimize,
-			.link_libc = true
-		})
-	});
+    const exe_check = b.addExecutable(.{ .name = name, .root_module = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true }) });
 
-	exe_check.root_module.addCSourceFiles(.{
-		.files = srcs,
-		.flags = &cflags
-	});
-	
-	const run_exe = b.addRunArtifact(exe);
+    exe_check.root_module.addCSourceFiles(.{ .files = srcs, .flags = &cflags });
+
+    const run_exe = b.addRunArtifact(exe);
     if (b.args) |args| run_exe.addArgs(args);
-	try add_build_steps(b, name, &exe_check.step, &run_exe.step);
+    try add_build_steps(b, name, &exe_check.step, &run_exe.step);
 }
 
 fn build_zig(b: *Build, main: Build.LazyPath, target: Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, name: []const u8) !void {
-	const exe = b.addExecutable(.{
+    const exe = b.addExecutable(.{
         .name = name,
         .root_module = b.createModule(.{
             .root_source_file = main,
@@ -86,7 +68,7 @@ fn build_zig(b: *Build, main: Build.LazyPath, target: Build.ResolvedTarget, opti
 
     const run_exe = b.addRunArtifact(exe);
     if (b.args) |args| run_exe.addArgs(args);
-	try add_build_steps(b, name, &exe_check.step, &run_exe.step);
+    try add_build_steps(b, name, &exe_check.step, &run_exe.step);
 }
 
 fn run_help2man(self: *Build.Step, opt: Build.Step.MakeOptions) !void {
@@ -95,10 +77,10 @@ fn run_help2man(self: *Build.Step, opt: Build.Step.MakeOptions) !void {
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const alloc = arena.allocator();
-	defer {
-		_ = arena.reset(.free_all);
-    	_ = arena.deinit();
-	}
+    defer {
+        _ = arena.reset(.free_all);
+        _ = arena.deinit();
+    }
 
     var help2man_result: proc.Child.RunResult = try proc.Child.run(.{ .allocator = alloc, .argv = &[_][]const u8{ "help2man", "zig-out/bin/lsf", "-o", "docs/lsf.1" } });
 
@@ -108,7 +90,7 @@ fn run_help2man(self: *Build.Step, opt: Build.Step.MakeOptions) !void {
 }
 
 pub fn build(b: *Build) !void {
-	cflags = if (b.release_mode == .off)
+    cflags = if (b.release_mode == .off)
         .{ "-std=c23", "-g", "-D_DEFAULT_SOURCE" }
     else
         .{ "-std=c23", "-fstack-protector-all", "-D_DEFAULT_SOURCE" };
@@ -119,7 +101,7 @@ pub fn build(b: *Build) !void {
     } });
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
-	// Global steps
+    // Global steps
     global_check = b.step("check", "Check if all apps compile");
     const fmt_step = b.step("fmt", "Format all zig code");
     const check_fmt_step = b.step("check-fmt", "Check formatting of all zig code");
@@ -137,12 +119,11 @@ pub fn build(b: *Build) !void {
     // First update versioning's on the C side.. //
     var fp: std.fs.File = try std.fs.cwd().openFile("src/app_info.h", .{ .mode = .write_only });
     // TODO: get this to grab `.version` in build.zig.zon //
-	try fp.writer().print("// This is a [Semantic Version](https://semver.org/).\nconst char vers[] = \"{s}\";", .{"0.0.0"});// catch {
+    try fp.writer().print("// This is a [Semantic Version](https://semver.org/).\nconst char vers[] = \"{s}\";", .{"0.0.0"}); // catch {
     fp.close();
-    
 
     // LSF
-    const lsf_src_files = [_][]const u8{"src/ls/main.c", "src/ctypes/strbuild.c", "src/ctypes/table.c"};
+    const lsf_src_files = [_][]const u8{ "src/ls/main.c", "src/ctypes/strbuild.c", "src/ctypes/table.c" };
     try build_c(b, &lsf_src_files, target, optimize, "lsf");
 
     // TOUCHF
