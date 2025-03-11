15a37dc1fede505b139f6c676a56a5180244239e
Eli <88557639+lishaduck@users.noreply.github.com>
2025-03-11 17:23:54 -0500

    style: prefer `return`-`switch` over `if`-`return`

--- src/file-io/file-io.zig
+++ src/file-io/file-io.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const posix = std.posix;
 const fs = std.fs;
 
-pub const OperationSettings = struct {
+pub const OperationSettings = packed struct {
     force: bool,
     recursive: bool,
     link: bool,
@@ -22,27 +22,31 @@ pub const OperationError = error{
 // these functions arent done, im just throwing things at the wall, we fail at rename() //
 pub fn move(src: []const u8, dest: []const u8, flags: OperationSettings) OperationError!void {
     _ = flags; // TODO
-    // long ass `if (err == error.x)` is because OperationError doesn't have every possible error. //
-    var src_fp: fs.Dir = fs.cwd().openDir(src, .{}) catch |err| {
-        if (err == error.AccessDenied) return error.AccessDenied;
-        if (err == error.BadPathName) return error.BadPathName;
-        if (err == error.SystemResources) return error.SystemResources;
-        if (err == error.FileNotFound) return error.FileNotFound;
-        if (err == error.FileBusy) return error.FileBusy;
-        return error.Unexpected;
+
+    var src_fp: fs.Dir = fs.cwd().openDir(src, .{}) catch |err| return switch (err) {
+        error.AccessDenied => error.AccessDenied,
+        error.BadPathName => error.BadPathName,
+        error.SystemResources => error.SystemResources,
+        error.FileNotFound => error.FileNotFound,
+
+        // `OperationError` doesn’t have every possible error.
+        else => return error.Unexpected,
     };
     defer src_fp.close();
+
     src_fp.rename(src, dest) catch return error.Unexpected;
 }
+
 pub fn remove(path: []const u8, flags: OperationSettings) OperationError!void {
     _ = flags; // TODO
-    fs.cwd().deleteFile(path, .{}) catch |err| {
-        if (err == error.AccessDenied) return error.AccessDenied;
-        if (err == error.BadPathName) return error.BadPathName;
-        if (err == error.SystemResources) return error.SystemResources;
-        if (err == error.FileBusy) return error.FileBusy;
-        if (err == error.FileNotFound) return error.FileNotFound;
-        if (err == error.IsDir) return error.IsDir;
-        return error.Unexpected;
+    fs.cwd().deleteFile(path, .{}) catch |err| return switch (err) {
+        error.AccessDenied => error.AccessDenied,
+        error.BadPathName => error.BadPathName,
+        error.SystemResources => error.SystemResources,
+        error.FileNotFound => error.FileNotFound,
+        error.FileBusy => error.FileBusy,
+        error.IsDir => error.IsDir,
+
+        else => return error.Unexpected,
     };
 }
