026914305c42d97f04ef3779a76a478526074900
Eli <88557639+lishaduck@users.noreply.github.com>
2025-08-25 02:10:10 +0000

    refactor: ansi should use fmt instead of fns

--- src/libcolor/colors.zig
+++ src/libcolor/colors.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Io = std.Io;
+const File = std.fs.File;
 
 pub const AnsiCode = enum {
     red,
@@ -12,42 +14,40 @@ pub const AnsiCode = enum {
     italic,
 
     reset,
+
+    pub fn format(this: @This(), writer: *Io.Writer) Io.Writer.Error!void {
+        _ = try writer.write(switch (this) {
+            .red => "\x1b[31m",
+            .blue => "\x1b[34m",
+            .cyan => "\x1b[36m",
+            .green => "\x1b[32m",
+            .yellow => "\x1b[33m",
+            .magenta => "\x1b[35m",
+
+            .bold => "\x1b[1m",
+            .italic => "\x1b[3m",
+
+            .reset => "\x1b[0m",
+        });
+    }
 };
 
-fn ansiCodeToString(ansi: AnsiCode) []const u8 {
-    return switch (ansi) {
-        .red => "\x1b[31m",
-        .blue => "\x1b[34m",
-        .cyan => "\x1b[36m",
-        .green => "\x1b[32m",
-        .yellow => "\x1b[33m",
-        .magenta => "\x1b[35m",
-
-        .bold => "\x1b[1m",
-        .italic => "\x1b[3m",
-
-        .reset => "\x1b[0m",
-    };
-}
-
-pub fn getEscapeCode(ansi: AnsiCode, stream: *std.fs.File) ?[]const u8 {
+pub fn writeEscapeCode(ansi: AnsiCode, stream: *File, writer: *Io.Writer) void {
     if (stream.getOrEnableAnsiEscapeSupport()) {
-        return ansiCodeToString(ansi);
+        ansi.format(writer) catch return;
     }
-
-    return null;
 }
 
 // based off std.debug.print() source code //
 // https://ziglang.org/documentation/0.14.0/std/#src/std/debug.zig //
-pub fn print(stream: *std.fs.File, ansi: AnsiCode, comptime fmt: []const u8, va_args: anytype) void {
+pub fn print(stream: *File, ansi: AnsiCode, comptime fmt: []const u8, va_args: anytype) void {
     var buffer: [1024]u8 = undefined;
     var stream_writer = stream.writer(&buffer);
     var writer = &stream_writer.interface;
 
-    nosuspend writer.print("{s}", .{getEscapeCode(ansi, stream) orelse ""}) catch return;
+    nosuspend writeEscapeCode(ansi, stream, writer);
     nosuspend writer.print(fmt, va_args) catch return;
-    nosuspend writer.print("{s}", .{getEscapeCode(AnsiCode.reset, stream) orelse ""}) catch return;
+    nosuspend writeEscapeCode(AnsiCode.reset, stream, writer);
 
     // This is the lazy way...
     nosuspend writer.flush() catch return;
